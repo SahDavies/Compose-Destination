cc0278bb4d9d5e7ec44449461398e57d3bafbe69
StudentOfKnowledge <dayodavies03@gmail.com>
2024-10-29 10:47:44 +0100

    Add inner class Utility that exposes two functions - requiredArgs, optionalArgs Add better safety for route construction. Handles cases such as: 1. mismatch in the number of required argument 2. mismatch in the number of optional argument 3. absent required argument 4. absent optional argument

--- Destination.kt
+++ Destination.kt
@@ -1,5 +1,6 @@
-private class Destinations(
+class Destination(
     routeId: String,
+    private val count: Int = 0,
     val namedArgs: List<NamedNavArgument> = emptyList()
 ) {
 
@@ -14,30 +15,30 @@ private class Destinations(
         }
     }
 
-    fun navRoute(block: Builder.() -> Unit = {}): String =
-        Builder(baseRoute = route.substringBefore('?'), namedNavArgs = namedArgs)
-            .apply(block)
-            .buildRoute()
+    fun navRoute(block: Utility.() -> Unit = {}): String = Utility().apply(block).buildRoute()
 
-    class Builder(
-        baseRoute: String,
-        private var namedNavArgs: List<NamedNavArgument> = emptyList()
-    ) {
-
-        private var requiredArgs: String = baseRoute
+    inner class Utility {
+        private var requiredArgs: String = this@Destination.route.substringBefore('?')
         private var optionalArgs: String = ""
-
         fun requiredArgs(vararg args: String) = apply {
-            /**
-             * TODO: Add safety if no required arg is passed or is incomplete
-             * TODO: also if function is called but route wasn't instantiated with required arg
-             * */
-            requiredArgs = requiredArgs.substringBefore('/') + args.joinToString(prefix = "/", separator = "/") { arg ->
-                "{$arg}"
-            }.trimEnd('/')
+            if (this@Destination.count != args.size) return@apply
+            requiredArgs = requiredArgs.substringBefore('/') + args.joinToString(prefix = "/", separator = "/").trimEnd('/')
         }
 
+        fun optionalArgs(vararg args: String?) = apply {
+            optionalArgs += this@Destination.namedArgs.zip(args).joinToString(prefix = "?", separator = "&") { (navArg, arg) ->
+                "${navArg.name}=$arg"
+            }.trimEnd('&', '?')
+        }
+        fun buildRoute(): String = requiredArgs + optionalArgs
+    }
+    class Builder(baseRoute: String) {
+        private var count = 0   // holds tally for required argument
+        private var requiredArgs: String = baseRoute
+        private var namedNavArgs: List<NamedNavArgument> = emptyList()
+
         fun withRequiredArgs(vararg args: String) = apply {
+            count = args.size
             requiredArgs += args.joinToString(prefix = "/", separator = "/") { arg ->
                 "{${arg}}"
             }.trimEnd('/')
@@ -47,19 +48,11 @@ private class Destinations(
             namedNavArgs = args.toList()
         }
 
-        fun optionalArgs(vararg args: String?) = apply {
-            optionalArgs += namedNavArgs.zip(args).joinToString(prefix = "?", separator = "&") { (navArg, arg) ->
-                "${navArg.name}=$arg"
-            }.trimEnd('&', '?')
-        }
-
-        fun build(): Destinations = Destinations(requiredArgs, namedNavArgs)
-
-        fun buildRoute(): String = requiredArgs + optionalArgs
+        fun build(): Destination = Destination(requiredArgs, count, namedNavArgs)
     }
 
     companion object {
-        fun generateRoute(routeID: String, block: Builder.() -> Unit = {}): Destinations =
+        fun generateRoute(routeID: String, block: Builder.() -> Unit = {}): Destination =
             Builder(routeID).apply(block).build()
     }
 }
